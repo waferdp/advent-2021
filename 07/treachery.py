9bc3516cc1b483fabd4b09fd34ceb1a470405987
Jacob Brännström <jacob.z.brannstrom@teliacompany.com>
2021-12-07 08:00:15 +0100

    7th part 2

--- 07/treachery.py
+++ 07/treachery.py
@@ -10,8 +10,8 @@ def createCostDict(high):
     costs = {}
     costs[0] = 0
     
-    for i in range(1, high):
-        costs[i] = costs[i-1] + 1
+    for i in range(1, high+1):
+        costs[i] = costs[i-1] + i
     return costs
 
 def bruteForceCost(crabs, costs):
@@ -20,7 +20,7 @@ def bruteForceCost(crabs, costs):
     lowestCost = None
 
     for i in range(low , high):
-        cost = calculateCost(crabs, i)
+        cost = calculateCost(crabs, costs, i)
         if lowestCost is None or cost < lowestCost:
             lowestCost = cost
     return lowestCost
@@ -35,56 +35,22 @@ def calculateCost(crabs, costs, position):
 def calcMiddle(large, small):
     return int( (large - small) / 2 )
 
-def findBestSpot(input):
-    smallest = min(input)
-    largest = max(input)
-    middle = calcMiddle(largest, smallest)
-    cost = calculateCost(input, middle)
-
-    leftCost = findRecurse(input, cost, smallest, middle, Direction.LEFT)
-    rightCost = findRecurse(input, cost, middle, largest, Direction.RIGHT)
-
-    return min([cost, leftCost, rightCost])
-
-def findRecurse(input, baseCost, small, large, lastdir):
-    
-    middle = calcMiddle(large, small)
-    cost = calculateCost(input, middle)
-
-    if baseCost <= cost:
-        if lastdir == Direction.LEFT:
-            if calcMiddle(large, middle) - middle < 1:
-                return baseCost
-            else:
-                rightCost = findRecurse(input, cost, middle, large, Direction.RIGHT)
-                return min([baseCost, rightCost])
-        elif lastdir == Direction.RIGHT:
-            if middle - calcMiddle(middle, small) < 1:
-                return baseCost
-            else:
-                leftCost = findRecurse(input, cost, small, middle, Direction.LEFT)
-                return min([baseCost, leftCost])
-    else:
-        leftCost = findRecurse(input, cost, small, middle, Direction.LEFT)
-        rightCost = findRecurse(input, cost, middle, large, Direction.RIGHT)
-        return min([cost, leftCost, rightCost])
-
 def test1():
     input = file.readSingleSeparated("test_input.txt")
     costs = createCostDict(max(input))
-    cost = calculateCost([16], 5)
+    cost = calculateCost([16], costs, 5)
     assert(cost == 66)
 
 def test2():
     input = file.readSingleSeparated("test_input.txt")
     costs = createCostDict(max(input))
-    cost = bruteForceCost(input)
-    assert(cost == 37)
+    cost = bruteForceCost(input, costs)
+    assert(cost == 168)
 
 def run():
     input = file.readSingleSeparated("puzzle_input.txt")
     costs = createCostDict(max(input))
-    cost = bruteForceCost(input)
+    cost = bruteForceCost(input, costs)
     return cost
 
 test1()
